b603982c127b15e431341c76cd5c70b30c8200c4
Adam Weigert <adweigert@wegmans.com>
2020-11-06 11:00:59 -0500

    added configure for publisher

--- src/ApplicationInsightsHealthCheckPublisherServiceCollectionExtensions.cs
+++ src/ApplicationInsightsHealthCheckPublisherServiceCollectionExtensions.cs
@@ -8,17 +8,18 @@ namespace Extensions.Microsoft.Diagnostics.HealthChecks.ApplicationInsights
 {
     public static class ApplicationInsightsHealthCheckPublisherServiceCollectionExtensions
     {
-        public static IHealthChecksBuilder AddApplicationInsightsPublisher(this IHealthChecksBuilder builder)
+        public static IHealthChecksBuilder AddApplicationInsightsPublisher(this IHealthChecksBuilder builder, Action<ApplicationInsightsHealthCheckPublisherOptions>? configure = null)
         {
             _ = builder ?? throw new ArgumentNullException(nameof(builder));
             builder.Services.AddOptions<ApplicationInsightsHealthCheckPublisherOptions>()
+                .Configure<IConfiguration>((options, configuration) => configuration.GetSection(nameof(ApplicationInsightsHealthCheckPublisher)).Bind(options))
                 .Configure(options =>
                 {
                     options.RunLocation = Environment.MachineName;
                     options.ApplicationName = Assembly.GetEntryAssembly().GetName().Name;
                     options.EnvironmentName = Environment.MachineName;
-                })
-                .Configure<IConfiguration>((options, configuration) => configuration.GetSection(nameof(ApplicationInsightsHealthCheckPublisher)).Bind(options));
+                    configure?.Invoke(options);
+                });
             builder.Services.AddSingleton<IHealthCheckPublisher, ApplicationInsightsHealthCheckPublisher>();
             return builder;
         }
